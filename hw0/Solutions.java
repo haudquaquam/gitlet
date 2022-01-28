bbb9fc612a53b43421b7c0d29e66648376cccfda
Rae Xin <raexin@berkeley.edu>
2022-01-28 17:50:31 -0600

    Finished hw

--- hw0/Solutions.java
+++ hw0/Solutions.java
@@ -7,8 +7,48 @@ public class Solutions {
      */
     public static boolean isEven(int x) {
         // TODO: Your code here. Replace the following return statement.
-        return false;
+        if (x % 2 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
     }
+
+    public static int max(int[] a) {
+        int maximum = a[0];
+        int i = 1;
+        while ((i < a.length) && (a[i] > maximum)) {
+            maximum = a[i];
+            i++;
+        }
+        return maximum;
+    }
+
+    public static boolean wordBank(String word, String[] bank) {
+        boolean flag = false;
+        for (int i = 0; i < bank.length; i++) {
+            if (word.equals(bank[i])) {
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    public static boolean threeSum(int[] a) {
+        boolean flag = false;
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                for (int k = 0; k < a.length; k++) {
+                    if ((a[i] + a[j] + a[k]) == 0) {
+                        flag = true;
+                    }
+                }
+            }
+        }
+        return flag;
+    }
+
 
     // TODO: Fill in the method signatures for the other exercises
     // Your methods should be static for this HW. DO NOT worry about what this means.
